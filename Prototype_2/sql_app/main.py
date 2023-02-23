ac5298aaddd4c7ed15d6d097dccbd0a6885ae10a
Casey <staple72@students.rowan.edu>
2023-02-23 17:48:25 -0500

    Added auth needed to access all end points

--- Prototype_2/sql_app/main.py
+++ Prototype_2/sql_app/main.py
@@ -110,7 +110,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
 
 
 @app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_active_user)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
@@ -118,13 +118,13 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 @app.get("/users/", response_model=List[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_active_user)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
 
 @app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
+def read_user(user_id: int, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_active_user)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -133,11 +133,11 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
 
 @app.post("/users/{user_id}/posts/", response_model=schemas.Post)
 def create_post_for_user(
-        user_id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
-    return crud.create_user_post(db=db, post=post, user_id=user_id)
+        post: schemas.PostCreate, user: schemas.User = Depends(get_current_active_user), db: Session = Depends(get_db), ):
+    return crud.create_user_post(db=db, post=post, user_id=user.id)
 
 
 @app.get("/posts/", response_model=List[schemas.Post])
-def read_posts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_posts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db), get_current_user: schemas.User = Depends(get_current_active_user)):
     posts = crud.get_posts(db, skip=skip, limit=limit)
     return posts
