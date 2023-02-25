6849dbc016f579326213d07c2b9be817f88cefb8
cStaples95 <74618860+cStaples95@users.noreply.github.com>
2023-02-24 19:25:34 -0500

    Reorganized repo, made two folders that contain each prototype's source files.

---
+++ Prototype-2/crud.py
@@ -0,0 +1,49 @@
+from sqlalchemy.orm import Session
+from passlib.hash import bcrypt
+
+import models
+import schemas
+
+
+def get_user(db: Session, user_id: int):
+    print(db.query(models.User).filter(models.User.id == user_id).first())
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    print(db.query(models.User).filter(models.User.email == email).first())
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_password = bcrypt.hash(user.password)
+    db_user = models.User(username=user.username,
+                          email=user.email, password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def get_posts(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Post).offset(skip).limit(limit).all()
+
+
+def create_user_post(db: Session, post: schemas.PostCreate, user_id: int):
+    db_post = models.Post(title=post.title, body=post.body, owner_id=user_id)
+    db.add(db_post)
+    db.commit()
+    db.refresh(db_post)
+    return db_post
+
+
+def verify_password(self, plain_password):
+    return bcrypt.verify(plain_password, self.password)
