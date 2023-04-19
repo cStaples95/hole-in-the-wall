79c169cea0e97385676bd9735773fd451e6da887
Penotb <brianmpenot@gmail.com>
2023-04-19 18:27:06 -0400

    Added navbar and UserSearchScreen
    Removed some of the temporary buttons used for testing
    Added the navbar and implemented it onto HomeScreen and UserSearchScreen
    Created UserSearchScreen's UI, still needs functionality

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,43 +1,16 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-import CustomButton from "../../components/CustomButton";
 import { useNavigation } from "@react-navigation/native";
+import BottomNavBar from "../../components/BottomNavBar/BottomNavBar";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
-
-  // ********** DELETE AFTER TESTING **********
-  const onProfileScreenPressed = () => {
-    navigation.navigate("Profile Screen");
-  };
-
-  const onReturnToSignInScreenPressed = () => {
-    navigation.navigate("Sign In Screen");
-  };
-
-  const onUserSearchScreenPressed = () => {
-    navigation.navigate("User Search Screen");
-  };
-  // ********** DELETE AFTER TESTING **********
 
   return (
     <View style={styles.root}>
       <Text style={styles.text}>Home Screen</Text>
 
-      <CustomButton
-        text="PLACEHOLDER: Profile Screen"
-        onPress={onProfileScreenPressed}
-      />
-
-      <CustomButton
-        text="PLACEHOLDER: Return to Sign In Screen"
-        onPress={onReturnToSignInScreenPressed}
-      />
-
-      <CustomButton
-        text="PLACEHOLDER: User Search Screen"
-        onPress={onUserSearchScreenPressed}
-        />
+        <BottomNavBar/>
     </View>
   );
 };
--- src/screens/UserSearchScreen/UserSearchScreen.js
+++ src/screens/UserSearchScreen/UserSearchScreen.js
@@ -1,24 +1,50 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, TextInput, Button, StyleSheet, Alert } from "react-native";
+import BottomNavBar from "../../components/BottomNavBar/BottomNavBar";
 
 const UserSearchScreen = () => {
+  const [userSearch, setUserSearch] = useState("");
+
+  const onPressedUserSearch = () => {
+    // Implement search logic using userSearch
+    Alert.alert(`Searching for: ${userSearch}`);
+  };
 
   return (
-    <View style={styles.root}>
-      <Text style={styles.text}>User Search Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Search users..."
+          value={userSearch}
+          onChangeText={setUserSearch}
+        />
+        <Button title="Search" onPress={onPressedUserSearch} />
+      </View>
+      <BottomNavBar />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  root: {
+  container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    padding: 16,
   },
-  text: {
-    fontSize: 20,
-    fontWeight: "bold",
+  searchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 16,
+  },
+  searchInput: {
+    flex: 1,
+    marginRight: 8,
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    backgroundColor: "white",
+    paddingHorizontal: 10,
   },
 });
+
 export default UserSearchScreen;
