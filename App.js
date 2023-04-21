1b9c6f5d4accecd70a5c8ee538361e0633fe2c27
Casey <74618860+cStaples95@users.noreply.github.com>
2023-04-20 20:28:48 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Navigation from "./src/navigation";
-import { ProfileScreen } from "./src/screens/ProfileScreen";
-import { ActivityFeed } from "./src/screens/Feed";
 
 export default function App() {
   return (
-    <ActivityFeed />
-    //<ProfileScreen/>
+    <View style={styles.container}>
+      <Navigation />
+    </View>
   );
 }
 
