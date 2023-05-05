02154808621c6eed039825006e7a8cb273275424
Matthew <100316043+CyberSkyGuy@users.noreply.github.com>
2023-05-04 23:36:09 -0400

    Add handling for cors

--- api/main.py
+++ api/main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, Request
 from typing import List, Union
 from sqlalchemy.orm import Session
 import authentication, schemas
 from database import models, database, email
-from routers import users, profiles, posts
+from routers import users, profiles, posts, comments 
+from fastapi.middleware.cors import CORSMiddleware
 # Casey Staples
 
 
@@ -12,15 +13,32 @@ models.Base.metadata.create_all(bind=database.engine)
 # Main point of app
 app = FastAPI()
 
-# allow for access from localhost
 origins = [
-    "http://localhost",
+    "http://localhost:19006",
 ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# allow access form everywhere
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    return response
 
 app.include_router(users.router, prefix="/users", tags=["users"])
 app.include_router(email.router, prefix="/emails", tags=["emails"])
 app.include_router(profiles.router, prefix="/profiles", tags=["profiles"])
 app.include_router(posts.router, prefix="/posts", tags=["posts"])
+app.include_router(comments.router, prefix="/comments", tags=["comments"])
 
 
 
