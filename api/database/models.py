ce9bdd567e9aafabaa7d385dd09f565c9979cf6d
Matthew <100316043+CyberSkyGuy@users.noreply.github.com>
2023-05-04 23:30:10 -0400

    Update database related functions and tables

--- api/database/models.py
+++ api/database/models.py
@@ -1,64 +1,75 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, BLOB
 from sqlalchemy.orm import relationship
 from .database import Base
 
-# Casey Staples
+# Casey Staples and Jonathan White
 # V .01
-# Models fo the database tables
+# Models for the database tables
 
 
 class User(Base):
     __tablename__ = "User"
 
-    userID = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String, index=True)
-    deleted = Column(Boolean, default=False)
+    UserID = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    Username = Column(String, unique=True, index=True)
+    Email = Column(String, unique=True, index=True)
+    Password = Column(String, index=True)
+    Deleted = Column(Boolean, default=False)
 
-
+# chnaged to add blob image and bio
 class Profile(Base):
     __tablename__ = "Profile"
 
-    profileID = Column(Integer, primary_key=True, index=True, autoincrement= True)
-    firstName = Column(String)
-    lastName = Column(String)
-    DOB = Column(String)
+    ProfileID = Column(Integer, primary_key=True, index=True, autoincrement= True)
+    UserID = Column(Integer, ForeignKey("User.UserID"))
+    Picture = Column(BLOB)
+    PictureExt = Column(String)
+    Bio = Column(String)
 
-    userID = Column(Integer, ForeignKey("User.userID"))
-
-
+    
 class UserSettings(Base):
     __tablename__ = "UserSettings"
 
-    userSettingsID = Column(Integer, primary_key=True, index=True)
-    commentsNotifs = Column(Boolean, default=True)
-    followNotifs = Column(Boolean, default=True)
+    UserSettingsID = Column(Integer, primary_key=True, index=True)
+    CommentsNotifs = Column(Boolean, default=True)
+    FollowNotifs = Column(Boolean, default=True)
 
-    userID = Column(Integer, ForeignKey("User.UserID"))
+    UserID = Column(Integer, ForeignKey("User.UserID"))
 
 
 class Comments(Base):
     __tablename__ = "Comments"
 
-    commentID = Column(Integer, primary_key=True, index=True)
-    comment = Column(String)
+    CommentID = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    Comment = Column(String)
     DateCommented = Column(String)
 
-    userID = Column(Integer, ForeignKey("User.userID"))
+    UserID = Column(Integer, ForeignKey("User.UserID"))
+    PostID = Column(Integer, ForeignKey("Post.PostID"))
+    
+class Likes(Base):
+    __tablename__ = "Likes"
+
+    LikeID = Column(Integer, primary_key=True, index=True, autoincrement=True)
+
+    UserID = Column(Integer, ForeignKey("User.UserID"))
     PostID = Column(Integer, ForeignKey("Post.PostID"))
 
-
+# added picture
 class Post(Base):
     __tablename__ = "Post"
 
     PostID = Column(Integer, primary_key=True, index=True, autoincrement=True)
     Title = Column(String)
-    Content = Column(String)
+    Description = Column(String)
     DatePosted = Column(String)
     Location = Column(String)
+    Picture = Column(BLOB)
+    PictureExt = Column(String)
 
-    userID = Column(Integer, ForeignKey("User.userID"))
+    UserID = Column(Integer, ForeignKey("User.UserID"))
+    Likes = relationship("Likes")
+    # TODO: foreign key for Likes and comments
 
 
 class PostTages(Base):
@@ -77,3 +88,39 @@ class Tags(Base):
     Tag = Column(String)
 
     PostTagsID = Column(Integer, ForeignKey("PostTags.PostTagsID"))
+
+# new models - Jonathan White
+
+class Friends(Base):
+    __tablename__ = "Friends"
+
+    FriendshipID = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    FriendUserID = Column(Integer)
+
+    UserID = Column(Integer, ForeignKey("User.UserID"))
+
+class Group(Base):
+    __tablename__ = "Group"
+
+    GroupID = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    GroupName = Column(String)
+
+    HangoutID = Column(Integer, ForeignKey("Hangout.HangoutID"))
+
+class GroupMembers(Base):
+    __tablename__ = "GroupMembers"
+
+    GroupMemberID = Column(Integer, primary_key=True, index=True, autoincrement=True)
+
+    UserID = Column(Integer, ForeignKey("User.UserID"))
+    GroupID = Column(Integer, ForeignKey("Group.GroupID"))
+
+class Hangout(Base):
+    __tablename__ = "Hangout"
+
+    HangoutID = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    HangoutName = Column(String)
+    Date = Column(String)
+    Votes = Column(Integer)
+
+    GroupID = Column(Integer, ForeignKey("Group.GroupID"))
